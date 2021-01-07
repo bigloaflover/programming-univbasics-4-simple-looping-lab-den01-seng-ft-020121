8fa21a56ac2c12143ca0f177598a041c4638e80c
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-07 05:04:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,7 @@
 def loop_message_five_times(message) 
-  counter = 0 
-  while message[counter] < 5 do 
+  while message < 5 do 
     puts message 
-    counter += 1
+  end 
 end 
 
 def loop_message_n_times
